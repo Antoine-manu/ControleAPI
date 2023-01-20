06f4d2f940ca21f659868fa3a8fcbb4b5fd3ccfa
Antoine-manu <agaudry.lamanu@gmail.com>
2023-01-20 13:55:12 +0100

    Init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Gettinng Started
+
+Apres avoir clonner le repo faite :
+
+## Installation des dependences
+- Npm Install
+## Base de donnée
+
+### Création de la base
+- npx sequelize-cli db:create
+
+### Lancement des migrations 
+- npx sequelize-cli db:migrate
+
+## Lancement de l'api 
+- npm start
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// require('express-group-routes');
+
+const app = express();
+
+// Body parsers
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+try {
+	app
+		.use('/api/v1', require('./routes/user'));
+
+	app.listen(6500, () => console.log('Server started: 6500'));
+} catch (error) {
+	console.log('error', error);
+}
