2eb08a08acebfd3b5e8af81ac61e040ab6e7fdf0
Antoine-manu <agaudry.lamanu@gmail.com>
2023-01-20 13:56:16 +0100

    Create reservation parking model

---
+++ models/reservations_parking.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Reservations_parking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Reservations_parking.init({
+    parking_id: DataTypes.INTEGER,
+    reservation_id: DataTypes.INTEGER,
+    start_date: DataTypes.DATE,
+    end_date: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Reservations_parking',
+  });
+  return Reservations_parking;
+};
