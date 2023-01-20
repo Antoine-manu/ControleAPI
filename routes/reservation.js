102e2a55ee0fbccaed3434dbe57e05bf7404394d
Antoine-manu <agaudry.lamanu@gmail.com>
2023-01-20 15:46:15 +0100

    route edit

--- routes/reservation.js
+++ routes/reservation.js
@@ -10,13 +10,13 @@ router.post('/reservation/create', reservationController.create);
 router.post('/reservation/find-all', reservationController.findAll);
 
 /* POST findOneById reservation. */
-router.post('/reservation/find-one-by-id', reservationController.findOneById);
+router.post('/reservation/find/:id', reservationController.findOneById);
 
 /* POST update reservation. */
 router.post('/reservation/update/:id', reservationController.update);
 
 /* POST delete reservation. */
-router.post('/reservation/delete', reservationController.delete);
+router.post('/reservation/delete/:id', reservationController.delete);
 
 
 module.exports = router;
